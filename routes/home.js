0a7721602c0e70dab466a693eb80442b9feffa5c
Shawn <pan.conan@gmail.com>
2019-12-02 00:43:14 +0800

    update home.js

--- routes/home.js
+++ routes/home.js
@@ -14,33 +14,6 @@ router.get('/', authenticated, (req, res) => {
             針對每個物件的date另存成shortDate轉成只有年月日
             */
             
-            /* ??助教，其實我本來是想要用下面這段，
-            將轉變成年月日的日期存到shortDate裡，
-            可就是存不進去，它只能讓我針對已有的key去改value而已。
-            但我有把下面這段簡化，在下面實驗，map取出陣列內的物件後，
-            該物件確實是可以任意插入新的key跟value進去，可為什麼這邊就不行了?
-            是因為eachRecord這個物件不是一般物件，沒辦法隨便插入key跟value進去?
-                        
-            let newAllRecords = allRecords.map((eachRecord) => {
-                let date = eachRecord.date
-                let shortD = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getDate().toString()
-                eachRecord.shortDate = shortD
-                console.log(eachRecord)
-                console.log(Object.getPrototypeOf(eachRecord))
-                return eachRecord
-            })
-            console.log(newAllRecords)
-            */
-
-            /*實驗map跟陣列型物件的使用
-            let x = [{name:'shawn', time:100},{name:'tim', time:99}]
-            let xx = x.map(item => {
-                item.address= 1
-                console.log(Object.getPrototypeOf(item))
-                return item
-            })
-            console.log(xx)
-            */
             Record.findOne({userId:req.user._id})
                 .sort({_id: -1})
                 .select({totalAmount: 1})
